66f319f397ed439572aab508508deb8cd76efaae
Snehashish1609 <snehashishlahiri1976@gmail.com>
2024-03-13 02:05:14 +0530

    test: add dummy api tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
+import json
 
 # Create your tests here.
+
+class GetDummyJsonViewTest(TestCase):
+    def test_get_dummy_json_view_with_params(self):
+        # Prepare request parameters
+        url = reverse('get_dummy_json')
+        params = {'name': 'Hello', 'email': 'hello@world.com'}
+        response = self.client.get(url, params)
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.content)
+
+        expected_data = {
+            "name": "Hello",
+            "email": "hello@world.com",
+            "other_data": "value",
+        }
+
+        self.assertDictEqual(response_data, expected_data)
+
+    def test_get_dummy_json_view_without_params(self):
+        url = reverse('get_dummy_json')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.content)
+
+        expected_data = {
+            "name": "",
+            "email": "",
+            "other_data": "value",
+        }
+
+        self.assertDictEqual(response_data, expected_data)
